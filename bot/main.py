ad088ecad2b8e4e9e6b777f513680ea2adc6d9c0
turecross321 <51852312+turecross321@users.noreply.github.com>
2023-01-22 19:12:36 +0100

    i dont even remember what i changed

--- bot/main.py
+++ bot/main.py
@@ -37,50 +37,52 @@ async def on_ready():
 
 @bot.tree.command(name="screenshot", description="sends a screenshot of what host is currently doing on their pc")
 async def Screenshot(interaction: discord.Interaction):
-
-    if (useCamera.lower() == "y"):
-        cam.read() # crank it on beforehands so the exposure isnt dfucked
-
     await interaction.response.defer()
 
-    print("The screenshot command has been triggered by " + str(interaction.user))
-    print("Attempting to play the notification sound effect.")
-
     try:
-        playsound('./screenshot_notification.wav')
+        if (useCamera.lower() == "y"):
+            cam.read() # crank it on beforehands so the exposure isnt dfucked
+
+
+        print("The screenshot command has been triggered by " + str(interaction.user))
+        print("Attempting to play the notification sound effect.")
+
+        try:
+            playsound('./screenshot_notification.wav')
+        except:
+            await interaction.response.send_message("Failed to play screenshot_notification.wav. Make sure it is in the same folder as the python file!")
+            return
+
+        screenshot_file_name = "./screenshot.png"
+        webcam_file_name = "./webcam.png"
+
+        if (useCamera.lower() == "y"):
+            result, image = cam.read()
+            cv2.imwrite(webcam_file_name, image)
+
+        image = pyautogui.screenshot()
+        image.save("./" + screenshot_file_name)
+
+        if (useCamera.lower() == "y"):
+            img1 = Image.open(screenshot_file_name)
+            img2 = Image.open(webcam_file_name)
+
+            webcamWidthPixelCountInRelationToHeight = img2.width / img2.height # i dont fucking know what to call this variable basically it stores how much longer the width is
+
+            screenshotWidth = img1.width
+
+            img2 = img2.resize((int(screenshotWidth / 3), int((screenshotWidth / 3) / webcamWidthPixelCountInRelationToHeight)), Image.LANCZOS)
+
+            img1.paste(img2, (0, 0))
+            img1.save("combined.png")
+
+            await interaction.followup.send(file=discord.File("./combined.png"))
+
+        else:
+            await interaction.followup.send(file=discord.File(screenshot_file_name))
+
+        print("Screenshot succesfully sent. (triggered by " + str(interaction.user) + ")")
     except:
-        await interaction.response.send_message("Failed to play screenshot_notification.wav. Make sure it is in the same folder as the python file!")
-        return
-
-    screenshot_file_name = "./screenshot.png"
-    webcam_file_name = "./webcam.png"
-
-    if (useCamera.lower() == "y"):
-        result, image = cam.read()
-        cv2.imwrite(webcam_file_name, image)
-
-    image = pyautogui.screenshot()
-    image.save("./" + screenshot_file_name)
-
-    if (useCamera.lower() == "y"):
-        img1 = Image.open(screenshot_file_name)
-        img2 = Image.open(webcam_file_name)
-
-        webcamWidthPixelCountInRelationToHeight = img2.width / img2.height # i dont fucking know what to call this variable basically it stores how much longer the width is
-
-        screenshotWidth = img1.width
-
-        img2 = img2.resize((int(screenshotWidth / 3), int((screenshotWidth / 3) / webcamWidthPixelCountInRelationToHeight)), Image.LANCZOS)
-
-        img1.paste(img2, (0, 0))
-        img1.save("combined.png")
-
-        await interaction.followup.send(file=discord.File("./combined.png"))
-
-    else:
-        await interaction.followup.send(file=discord.File(screenshot_file_name))
-
-    print("Screenshot succesfully sent. (triggered by " + str(interaction.user) + ")")
-
+        await interaction.followup.send("piece of shit errored try again")
 
 bot.run(bot_token)
